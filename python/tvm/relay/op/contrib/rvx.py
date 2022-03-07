b0d8c3243cdfa9a962c09047befcbe8859d84b66
特里斯坦.邱 <8440214+tristanqiu@user.noreply.gitee.com>
2022-03-07 18:00:49 +0800

    finish rvx patterns (w.o. check)

--- python/tvm/relay/op/contrib/rvx.py
+++ python/tvm/relay/op/contrib/rvx.py
@@ -15,7 +15,7 @@
 # specific language governing permissions and limitations
 # under the License.
 # pylint: disable=invalid-name, unused-argument
-"""RVX library supported operators and patterns"""
+"""RVX Hardware supported operators and patterns"""
 import logging
 
 import tvm.ir
@@ -27,7 +27,7 @@ from .register import register_pattern_table
 
 logger = logging.getLogger("RVX")
 
-def make_conv_pattern(with_relu, with_pool, relu_first):
+def make_conv_pattern(with_bias, with_relu, with_pool, relu_first=True):
     data = wildcard()
     weight = wildcard()
     bias = wildcard()
@@ -39,7 +39,7 @@ def make_conv_pattern(with_relu, with_pool, relu_first):
     reqt_in_zp = wildcard()
     reqt_out_scale = wildcard()
     reqt_out_zp = wildcard()
-    conv_out = is_op("qnn.conv2d")(
+    conv = is_op("qnn.conv2d")(
         data,
         weight,
         in_zp,
@@ -47,9 +47,25 @@ def make_conv_pattern(with_relu, with_pool, relu_first):
         in_scale,
         kernel_scale
     )
+    # add possible nn.bias_add
+    if with_bias:
+        conv_out = is_op("nn.bias_add")(
+            conv,
+            bias
+        )
+    else:
+        conv_out = conv
+    # add qnn.requantize
+    conv_req = is_op("qnn.requantize")(
+        conv_out,
+        reqt_in_scale,
+        reqt_in_zp,
+        reqt_out_scale,
+        reqt_out_zp
+    )
     if relu_first:
         if with_relu:
-            relu_out = is_op("clip")(conv_out)
+            relu_out = is_op("clip")(conv_req)
         else:
             relu_out = conv_out
 
@@ -60,7 +76,7 @@ def make_conv_pattern(with_relu, with_pool, relu_first):
         return pool_out
     else:
         if with_pool:
-            pool_out = is_op("nn.max_pool2d")(conv_out)
+            pool_out = is_op("nn.max_pool2d")(conv_req)
         else:
             pool_out = conv_out
 
@@ -69,6 +85,18 @@ def make_conv_pattern(with_relu, with_pool, relu_first):
         else:
             relu_out = pool_out
         return relu_out
+
+# make_conv_pattern(with_bias, with_relu, with_pool, relu_first=True)
+conv_wob_woa_wop_pat = ("rvx.conv_wob_woa_wop", make_conv_pattern(False, False, False))
+conv_wb_woa_wop_pat = ("rvx.conv_wb_woa_wop", make_conv_pattern(True, False, False))
+conv_wob_wa_wop_pat = ("rvx.conv_wob_wa_wop", make_conv_pattern(False, True, False))
+conv_wb_wa_wop_pat = ("rvx.conv_wob_wa_wop", make_conv_pattern(True, True, False))
+conv_wob_woa_wp_pat = ("rvx.conv_wob_woa_wp", make_conv_pattern(False, False, True))
+conv_wb_woa_wp_pat = ("rvx.conv_wb_woa_wp", make_conv_pattern(True, False, True))
+conv_wob_wa_wp_pat = ("rvx.conv_wob_wa_wp", make_conv_pattern(False, True, True))
+conv_wb_wa_wp_pat = ("rvx.conv_wb_wa_wp", make_conv_pattern(True, True, True))
+conv_wob_wp_wa_pat = ("rvx.conv_wob_wp_wa", make_conv_pattern(False, True, True, False))
+conv_wb_wp_wa_pat = ("rvx.conv_wb_wp_wa", make_conv_pattern(True, True, True, False))
 
 @register_pattern_table("rvx")
 def pattern_table():
@@ -79,5 +107,38 @@ def pattern_table():
     rvx_patterns : List[rvx_pattern]
         Created patterns.
     """
-    rvx_patterns = []
-    return rvx_patterns
+    patterns = [conv_wb_wa_wp_pat, conv_wb_wp_wa_pat,
+                conv_wb_wa_wop_pat, conv_wb_woa_wp_pat, conv_wob_wa_wp_pat, conv_wob_wp_wa_pat,
+                conv_wb_woa_wop_pat, conv_wob_wa_wop_pat, conv_wob_woa_wp_pat,
+                conv_wob_woa_wop_pat]
+    return patterns
+
+def partition_for_rvx(mod, params=None):
+    """Partition the graph greedily offloading supported operators to RVX.
+
+    Parameters
+    ----------
+    mod : Module
+        The module to run passes on.
+    params : Optional[Dict[str, NDArray]]
+        Constant input parameters.
+    Returns
+    -------
+    mod : Module
+        Annotated and partitioned module.
+    """
+    if params:
+        mod["main"] = bind_params_by_name(mod["main"], params)
+    seq = tvm.transform.Sequential(
+        [
+        [
+            transform.InferType(),
+            transform.MergeComposite(pattern_table()),
+            transform.AnnotateTarget("rvx"),
+            transform.MergeCompilerRegions(),
+            transform.PartitionGraph(),
+        ]
+        ]
+    )
+
+    return seq(mod)
